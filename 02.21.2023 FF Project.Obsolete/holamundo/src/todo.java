9b6810d9b201309891d2a676b3c09b8b578b45c0
IramMcFly <124701863+IramMcFly@users.noreply.github.com>
2023-03-02 09:50:59 -0600

    Update todo.java

--- 02.21.2023 FF Project.Obsolete/holamundo/src/todo.java
+++ 02.21.2023 FF Project.Obsolete/holamundo/src/todo.java
@@ -1,5 +1,17 @@
 public class todo {
+  
+  //resestructurar el codigo - listo
+ //quitar el sistema de lectura de database: errores de lectura - listo
+//disenar la GUI - PENDIENTE
+ //quitar los archivos de imagen (persobajes y fondos) de la carpeta src - Listo
+ //revisar el sistema de gestion de archivos //posiblemente eliminado - listo
+ //limpiar el codigo para la expo - (1/2 listo) /horas gastadas: 2 
+ //pasar el proyecto a netbeans (Eclipse tiene problemas de GUI) - jajaja no funciona //corregir nombres y paquetes - PENDIENTE
+ //VSC dejo de crear GUI - XD
+ //
  
-
+ 
+ 
+ //darse de baja 
 
 }
